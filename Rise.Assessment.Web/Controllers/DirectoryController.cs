ad1a662f1a6407fc03e85d751e484c49473b1beb
Mehmet Yıldız <yildzmehmet7@gmail.com>
2021-02-21 22:29:37 +0300

    Directory Controller added

---
+++ Rise.Assessment.Web/Controllers/DirectoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rise.Assessment.Web.Controllers
+{
+    public class DirectoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
